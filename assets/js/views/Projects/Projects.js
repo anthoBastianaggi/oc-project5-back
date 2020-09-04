b8078b158f31ea847a82faf5097393a3669428c7
anthoBastianaggi <a.bastianaggi@gmail.com>
2020-09-04 16:37:36 +0200

    [PO-34] (Add) Create page Projects

--- assets/js/views/Projects/Projects.js
+++ assets/js/views/Projects/Projects.js
@@ -1,12 +1,32 @@
-import React, {Component} from 'react';
+import React from 'react';
+import './Projects.module.scss';
+import List from '../../components/List/List';
     
-class Project extends Component {
+const Projects = () => {
+    const data = [
+        { id: "2", title: "WebAgency", category: "Sites Vitrines" },
+        { id: "3", title: "Blog de Jean Forteroche", category: "Web Develop" },
+        { id: "4", title: "VeloV : Réservation de vélo", category: "App Web" }
+    ];
     
-    render() {
-        return (
-           <div>Bienvenue sur la page Project !</div>
-        )
-    }
+    return (
+        <div className="wrap">
+            <h1 className="projects-title">Projects</h1>
+            <List 
+            data={data} 
+            children={
+                <div className="list-header">
+                    <div className="title">
+                        <span>Titre</span>
+                    </div>
+                    <div className="category">
+                        <span>Categories</span>
+                    </div>
+                </div>
+            } 
+            />
+        </div>
+    )
 }
     
-export default Project;
+export default Projects;
