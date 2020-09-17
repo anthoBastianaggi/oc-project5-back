139a7496a131267263f090f448ecc3f86b597988
anthoBastianaggi <a.bastianaggi@gmail.com>
2020-09-17 10:23:17 +0200

    [PO-41] (Add) Create page add service

--- assets/js/views/Services/add/AddService.js
+++ assets/js/views/Services/add/AddService.js
@@ -1,12 +1,34 @@
-import React, {Component} from 'react';
+import React from 'react';
+import './AddService.module.scss';
+import Button from '../../../components/Button/Button';
     
-class AddService extends Component {
-    
-    render() {
-        return (
-           <div>Bienvenue sur la page ajout service !</div>
-        )
-    }
+const AddService = () => {
+    return (
+        <div className="wrap">
+            <div className="service-header">
+                <div className="service-button">
+                    <div className="button-add-service">
+                        <Button type="primary" label="Publier" className="btn-add-service" />
+                    </div>
+                </div>     
+                <div className="service-title">
+                    <h1 className="title">Ajouter un nouveau service</h1>
+                </div>  
+            </div>
+            <div className="content-service">
+                <div className="content-edit">
+                    <div className="title">
+                        <h2>Titre</h2>
+                        <input placeholder="Ajouter un titre" />
+                    </div>
+                    <div className="description">
+                        <h2>Description</h2>
+                        <textarea placeholder="Ajouter une description"  />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
     
 export default AddService;
